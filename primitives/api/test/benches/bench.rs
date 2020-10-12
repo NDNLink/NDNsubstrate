4c9cf2c921e5162c2734767ffe23d0a08ec84e17
Alan <61774141+Alanshaw2048@users.noreply.github.com>
2020-10-12 14:36:56 +0800

    Update bench.rs

--- primitives/api/test/benches/bench.rs
+++ primitives/api/test/benches/bench.rs
@@ -15,6 +15,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+
 use criterion::{Criterion, criterion_group, criterion_main};
 use substrate_test_runtime_client::{
 	DefaultTestClientBuilderExt, TestClientBuilder,
