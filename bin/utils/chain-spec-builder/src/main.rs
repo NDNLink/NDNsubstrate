d7e7ec711a406524ac370f210e12faa37b18c1b8
Alex <70995153+AlexBentov@users.noreply.github.com>
2020-10-26 16:17:34 +0800

    Update main.rs

--- bin/utils/chain-spec-builder/src/main.rs
+++ bin/utils/chain-spec-builder/src/main.rs
@@ -16,6 +16,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+
 use std::{fs, path::{Path, PathBuf}};
 
 use ansi_term::Style;
