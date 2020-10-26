d393b1688dbb22df34854859475aefd9f7c42a72
Alex <70995153+AlexBentov@users.noreply.github.com>
2020-10-26 16:20:00 +0800

    Update lib.rs

--- bin/utils/subkey/src/lib.rs
+++ bin/utils/subkey/src/lib.rs
@@ -16,6 +16,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+
 use structopt::StructOpt;
 use sc_cli::{
 	Error, VanityCmd, SignCmd, VerifyCmd, InsertCmd,
